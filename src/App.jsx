873c0466c8f3c6749b99632b03f8738013978d26
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-13 22:27:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import DashboardPage from "./pages/DashboardPage";
-import CuotasPage from "./pages/CuotasPage";
-import SociosPage from "./pages/SociosPage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { CuotasPage } from "./pages/CuotasPage";
+import { SociosPage } from "./pages/SociosPage";
 
 export default function App() {
   return (
