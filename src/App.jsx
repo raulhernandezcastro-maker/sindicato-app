3096a5d95fbae7c77ae2e59d8ceda6fb8fa4ef9f
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-13 23:07:22 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./contexts/AuthContext";
+import { AppLayout } from "./components/layout/AppLayout";
+
 import { DashboardPage } from "./pages/DashboardPage";
 import CuotasPage from "./pages/CuotasPage";
 import { SociosPage } from "./pages/SociosPage";
-
-import { AppLayout } from "./components/layout/AppLayout";
-import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <AppLayout>
-          <Routes>
+        <Routes>
+          {/* Layout principal */}
+          <Route element={<AppLayout />}>
             <Route path="/" element={<DashboardPage />} />
             <Route path="/cuotas" element={<CuotasPage />} />
             <Route path="/socios" element={<SociosPage />} />
-          </Routes>
-        </AppLayout>
+          </Route>
+        </Routes>
       </BrowserRouter>
     </AuthProvider>
   );
