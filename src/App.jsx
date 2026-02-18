5c4ff59d94e53f2ef4cff5a0685cf809904799d9
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-17 21:46:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AppLayout } from './components/layout/AppLayout'
 
-// TODAS estas páginas son NAMED exports (según tus archivos reales)
-import { DashboardPage } from './pages/DashboardPage'
-import { AvisosPage } from './pages/AvisosPage'
-import { DocumentosPage } from './pages/DocumentosPage'
-import { PerfilPage } from './pages/PerfilPage'
-import { SociosPage } from './pages/SociosPage'
-
-// Esta SÍ es default (Cuotas)
+import DashboardPage from './pages/DashboardPage'
+import AvisosPage from './pages/AvisosPage'
+import DocumentosPage from './pages/DocumentosPage'
+import PerfilPage from './pages/PerfilPage'
+import SociosPage from './pages/SociosPage'
 import CuotasPage from './pages/CuotasPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-
-          <Route path="/avisos" element={<AvisosPage />} />
-          <Route path="/documentos" element={<DocumentosPage />} />
-          <Route path="/perfil" element={<PerfilPage />} />
-
-          <Route path="/cuotas" element={<CuotasPage />} />
-          <Route path="/socios" element={<SociosPage />} />
-        </Route>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/avisos" element={<AvisosPage />} />
+        <Route path="/documentos" element={<DocumentosPage />} />
+        <Route path="/perfil" element={<PerfilPage />} />
+        <Route path="/cuotas" element={<CuotasPage />} />
+        <Route path="/socios" element={<SociosPage />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
