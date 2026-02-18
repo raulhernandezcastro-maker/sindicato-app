74378634849d04c9c267b7695d7580ef19ded0b5
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-17 21:23:00 -0300

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { DesktopNav } from './DesktopNav';
-import { MobileNav } from './MobileNav';
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { DesktopNav } from './DesktopNav'
+import { MobileNav } from './MobileNav'
 
-export function AppLayout({ children }) {
+export function AppLayout() {
   return (
     <div className="min-h-screen bg-background">
+      {/* Menú lateral escritorio */}
       <DesktopNav />
+
+      {/* Contenido principal */}
       <main className="md:ml-64 pb-20 md:pb-0">
         <div className="container mx-auto p-4 md:p-6 lg:p-8">
-          {children}
+          {/* 🔑 AQUÍ SE RENDERIZAN LAS PÁGINAS */}
+          <Outlet />
         </div>
       </main>
+
+      {/* Menú móvil */}
       <MobileNav />
     </div>
-  );
+  )
 }
