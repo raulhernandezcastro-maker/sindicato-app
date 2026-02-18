52a7090bd5e401ded44aa80c7df18b3962344ec1
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-17 21:28:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import { AppLayout } from './components/layout/AppLayout'
 
+// Páginas
 import { DashboardPage } from './pages/DashboardPage'
-import AvisosPage from './pages/AvisosPage'
-import DocumentosPage from './pages/DocumentosPage'
-import PerfilPage from './pages/PerfilPage'
+import { AvisosPage } from './pages/AvisosPage'
+import { DocumentosPage } from './pages/DocumentosPage'
+import { PerfilPage } from './pages/PerfilPage'
 import CuotasPage from './pages/CuotasPage'
 import { SociosPage } from './pages/SociosPage'
 
@@ -13,10 +14,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* 🔐 Layout principal con menú */}
+        {/* 🔑 TODA LA APP CON LAYOUT */}
         <Route element={<AppLayout />}>
-
           <Route path="/" element={<DashboardPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
 
@@ -26,12 +25,10 @@ export default function App() {
 
           <Route path="/cuotas" element={<CuotasPage />} />
           <Route path="/socios" element={<SociosPage />} />
-
         </Route>
 
         {/* fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
-
       </Routes>
     </BrowserRouter>
   )
