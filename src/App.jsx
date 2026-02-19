c971f5cdf455f57e19efaf43bde198133e3e0309
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-18 21:20:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,36 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import { AppLayout } from './components/layout/AppLayout'
-import { RoleRoute } from './components/auth/RoleRoute'
 
-// Páginas
-import HomePage from './pages/HomePage'
+// Pages (TODAS son named exports)
 import { DashboardPage } from './pages/DashboardPage'
 import { AvisosPage } from './pages/AvisosPage'
 import { DocumentosPage } from './pages/DocumentosPage'
 import { PerfilPage } from './pages/PerfilPage'
 import { SociosPage } from './pages/SociosPage'
-import CuotasPage from './pages/CuotasPage'
 import { LoginPage } from './pages/LoginPage'
+
+// Cuotas es la ÚNICA con default export
+import CuotasPage from './pages/CuotasPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* LOGIN */}
+        {/* Login (sin layout) */}
         <Route path="/login" element={<LoginPage />} />
 
-        {/* ÁREA PROTEGIDA */}
+        {/* Layout principal con menú */}
         <Route element={<AppLayout />}>
-          {/* Acceso general */}
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+
           <Route path="/avisos" element={<AvisosPage />} />
           <Route path="/documentos" element={<DocumentosPage />} />
           <Route path="/perfil" element={<PerfilPage />} />
 
-          {/* Director + Admin */}
-          <Route
-            path="/dashboard"
-            element={
-              <RoleRoute allow={['director', 'administrador']}>
-                <DashboardPage />
-              </RoleRoute>
-            }
-          />
-
-          <Route
-            path="/cuotas"
-            element={
-              <RoleRoute allow={['director', 'administrador']}>
-                <CuotasPage />
-              </RoleRoute>
-            }
-          />
-
-          {/* SOLO Admin */}
-          <Route
-            path="/socios"
-            element={
-              <RoleRoute allow={['administrador']}>
-                <SociosPage />
-              </RoleRoute>
-            }
-          />
+          <Route path="/cuotas" element={<CuotasPage />} />
+          <Route path="/socios" element={<SociosPage />} />
         </Route>
 
         {/* Fallback */}
