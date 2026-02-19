f87e128cfb0221a86edd57cd8865bc6c54681ce1
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-02-18 21:44:07 -0300

    Update AvisosPage.jsx

--- src/pages/AvisosPage.jsx
+++ src/pages/AvisosPage.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+import { AppLayout } from '../components/layout/AppLayout'
 
+export function AvisosPage() {
+  return (
+    <AppLayout>
+      <div>
+        <h1>Avisos</h1>
+        <p>Página de avisos (test)</p>
+      </div>
+    </AppLayout>
+  )
+}
