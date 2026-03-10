bb597a86eb816264d15ff2c56aa80f7951ef19a7
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-03-10 16:31:57 -0300

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -9,16 +9,16 @@ export function AppLayout() {
       {/* Menú lateral escritorio */}
       <DesktopNav />
 
+      {/* Menú móvil (incluye header superior + barra inferior + offsets) */}
+      <MobileNav />
+
       {/* Contenido principal */}
-      <main className="md:ml-64 pb-20 md:pb-0">
+      {/* pt-16 móvil = barra superior | pb-20 móvil = barra inferior */}
+      <main className="md:ml-64 pt-16 pb-20 md:pt-0 md:pb-0">
         <div className="container mx-auto p-4 md:p-6 lg:p-8">
-          {/* 🔑 AQUÍ SE RENDERIZAN LAS PÁGINAS */}
           <Outlet />
         </div>
       </main>
-
-      {/* Menú móvil */}
-      <MobileNav />
     </div>
   )
 }
