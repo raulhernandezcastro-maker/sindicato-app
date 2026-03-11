90f24cc8bb75807297d4a6f25e849a59195120e3
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-03-10 21:09:52 -0300

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -14,7 +14,7 @@ export function AppLayout() {
 
       {/* Contenido principal */}
       {/* pt-16 móvil = barra superior | pb-20 móvil = barra inferior */}
-      <main className="md:ml-64 pt-16 pb-20 md:pt-0 md:pb-0">
+      <main className="md:ml-64 pt-20 pb-20 md:pt-0 md:pb-0">
         <div className="container mx-auto p-4 md:p-6 lg:p-8">
           <Outlet />
         </div>
