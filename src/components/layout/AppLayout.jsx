c4d50d4f711bc2a99c787a422d06334dbd1e5334
raulhernandezcastro-maker <raul.hernandez.castro@gmail.com>
2026-03-18 16:36:51 -0300

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { DesktopNav } from './DesktopNav'
 import { MobileNav } from './MobileNav'
 import { useNotifications } from '../../hooks/useNotifications'
@@ -7,6 +7,9 @@ import { useNotifications } from '../../hooks/useNotifications'
 export function AppLayout() {
   // Registrar token FCM al cargar la app
   useNotifications()
+
+  // Usar la ruta actual como key para forzar remonte de cada página
+  const location = useLocation()
 
   return (
     <div className="min-h-screen bg-background">
@@ -20,7 +23,7 @@ export function AppLayout() {
       {/* pt-16 móvil = barra superior | pb-20 móvil = barra inferior */}
       <main className="md:ml-64 pt-20 pb-20 md:pt-0 md:pb-0">
         <div className="container mx-auto p-4 md:p-6 lg:p-8">
-          <Outlet />
+          <Outlet key={location.pathname} />
         </div>
       </main>
     </div>
